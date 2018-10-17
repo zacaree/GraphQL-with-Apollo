8c476e8d5b1e9cdf6015157211592f8835a4a14d
zacaree <zacharem@gmail.com>
2018-10-17 13:56:59 -0500

    React is pulling in data from GraphQL endpoint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
+// gql takes the query strings and turns them into something we can actually use.
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,24 +11,42 @@ const client = new ApolloClient({
   uri: 'https://api-useast.graphcms.com/v1/cjnd6rhce5tvu01dgef5a6juy/master'
 });
 
+// Writing a query
+const POSTS_QUERY = gql`
+  {
+    posts {
+      id
+      createdAt
+      title
+      body
+    }
+  }
+`;
+
 class App extends Component {
   render() {
     return (
+
+      // Apollo Provider wraps our application
       <ApolloProvider client={client}>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+
+            {/* Here's the data from POSTS_QUERY in use via a render prop */}
+            <Query query={POSTS_QUERY}>
+              {({loading, data}) => {
+                if (loading) return 'Loading...';
+                const { posts } = data;
+                return posts.map(post => (
+                  <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                  </div>
+                ))
+              }}
+            </Query>
+
           </header>
         </div>
       </ApolloProvider>
