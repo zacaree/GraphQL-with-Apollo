b9e64ea955239176e9857ecf8ab7b295948cab9c
Zac <zacaree@gmail.com>
2018-10-18 16:24:44 -0500

    Broke form into it's own component

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -4,59 +4,19 @@ import gql from 'graphql-tag';
 import PostForm from './PostForm'
 
 export default class NewPost extends Component {
-  state = {
-    title: '',
-    body: ''
-  }
 
-  handleInput = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
 
   render() {
-    const { title, body } = this.state;
+
     return (
       <Fragment>
         <h3>Create a Post</h3>
-        <Mutation
-          mutation={NEW_POST}
-          // These variables have a "$" below but we don't want that here.
-          variables={{
-            title: this.state.title,
-            body: this.state.body
-          }}
-        >
+        <Mutation mutation={NEW_POST}>
           {/* The function "createPost" is being passed into this function so that it can be used in the form. */}
           {createPost => (
-            // The submission returns a promise. Then we want to clear the form.
-            <form onSubmit={(e) => {
-              e.preventDefault();
-              createPost().then(() => {
-                this.setState({
-                  title: '',
-                  body: ''
-                });
-              }).catch(e => console.log(e));
-            }}>
-              <input
-                type="text"
-                name="title"
-                onChange={this.handleInput}
-                value={title}
-                placeholder="Post title"
-              />
-              <textarea
-                type="text"
-                name="body"
-                onChange={this.handleInput}
-                value={body}
-                placeholder="Post body"
-              />
-              <button>Submit</button>
-            </form>
+            <PostForm onSubmit={createPost} />
           )}
         </Mutation>
-        {/* <PostForm /> */}
       </Fragment>
     )
   }
--- src/Posts/PostForm.js
+++ src/Posts/PostForm.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
 
 export default class PostForm extends Component {
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
+    const { onSubmit } = this.props;
+    const { title, body } = this.state;
+
     return (
-      <form>
-        <input type="text" placeholder="Post title"/>
-        <textarea type="text" placeholder="Post body"/>
+      // The submission returns a promise. Then we want to clear the form.
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit({
+          // These variables have a "$" below but we don't want that here.
+          variables: {
+            title: this.state.title,
+            body: this.state.body
+          }
+        })
+          .then(() => {
+            this.setState({
+              title: '',
+              body: ''
+            });
+          }).catch(e => console.log(e));
+      }}>
+        <input
+          type="text"
+          name="title"
+          onChange={this.handleInput}
+          value={title}
+          placeholder="Post title"
+        />
+        <textarea
+          type="text"
+          name="body"
+          onChange={this.handleInput}
+          value={body}
+          placeholder="Post body"
+        />
         <button>Submit</button>
       </form>
     )
