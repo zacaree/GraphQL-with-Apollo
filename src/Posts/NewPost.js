135af059354f91f00e882a79007ca054f25b6a59
zacaree <zacharem@gmail.com>
2018-10-17 21:57:12 -0500

    EOD: Building a form that will post using a GraphQL mutation.

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -1,11 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import PostForm from './PostForm'
 
 export default class NewPost extends Component {
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
+    const {title, body} = this.state;
     return (
-      <div>
-        
-      </div>
+      <Fragment>
+        <h3>Create a Post</h3>
+        <Mutation
+          mutation={NEW_POST}
+          variables={{
+            title,
+            body
+            // 14 minutes into lesson 12
+          }}
+        >
+          <form>
+            <input 
+              type="text" 
+              name="title" 
+              onChange={this.handleInput} 
+              value={title} 
+              placeholder="Post title" 
+            />
+            <textarea 
+              type="text" 
+              name="body" 
+              onChange={this.handleInput} 
+              value={body} 
+              placeholder="Post body" 
+            />
+            <button>Submit</button>
+          </form>
+        </Mutation>
+        {/* <PostForm /> */}
+      </Fragment>
     )
   }
 }
+
+// After addPost we're defining our variables – $title (variable) will be a string and it's required (!)
+const NEW_POST = gql`
+  mutation addPost($title: String!, $body: String!) {
+    createPost(data: {
+      status: PUBLISHED
+      title: $title
+      body: $body
+    }) {
+      title
+      body
+      id
+    }
+  }
+`;
--- src/Posts/PostForm.js
+++ src/Posts/PostForm.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class PostForm extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <form>
+        <input type="text" placeholder="Post title"/>
+        <textarea type="text" placeholder="Post body"/>
+        <button>Submit</button>
+      </form>
     )
   }
 }
