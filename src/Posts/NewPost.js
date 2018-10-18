75f7822adab499435448985d451e5b2d3eb52112
Zac <zacaree@gmail.com>
2018-10-18 12:55:00 -0500

    We can now post data to our GraphQL endpoint through a form!

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -14,35 +14,47 @@ export default class NewPost extends Component {
   }
 
   render() {
-    const {title, body} = this.state;
+    const { title, body } = this.state;
     return (
       <Fragment>
         <h3>Create a Post</h3>
         <Mutation
           mutation={NEW_POST}
+          // These variables have a "$" below but we don't want that here.
           variables={{
-            title,
-            body
-            // 14 minutes into lesson 12
+            title: this.state.title,
+            body: this.state.body
           }}
         >
-          <form>
-            <input 
-              type="text" 
-              name="title" 
-              onChange={this.handleInput} 
-              value={title} 
-              placeholder="Post title" 
-            />
-            <textarea 
-              type="text" 
-              name="body" 
-              onChange={this.handleInput} 
-              value={body} 
-              placeholder="Post body" 
-            />
-            <button>Submit</button>
-          </form>
+          {/* The function "createPost" is being passed into this function so that it can be used in the form. */}
+          {createPost => (
+            // The submission returns a promise. Then we want to clear the form.
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              createPost().then(() => {
+                this.setState({
+                  title: '',
+                  body: ''
+                });
+              }).catch(e => console.log(e));
+            }}>
+              <input
+                type="text"
+                name="title"
+                onChange={this.handleInput}
+                value={title}
+                placeholder="Post title"
+              />
+              <textarea
+                type="text"
+                name="body"
+                onChange={this.handleInput}
+                value={body}
+                placeholder="Post body"
+              />
+              <button>Submit</button>
+            </form>
+          )}
         </Mutation>
         {/* <PostForm /> */}
       </Fragment>
@@ -52,7 +64,7 @@ export default class NewPost extends Component {
 
 // After addPost we're defining our variables – $title (variable) will be a string and it's required (!)
 const NEW_POST = gql`
-  mutation addPost($title: String!, $body: String!) {
+  mutation createPost($title: String!, $body: String!) {
     createPost(data: {
       status: PUBLISHED
       title: $title
