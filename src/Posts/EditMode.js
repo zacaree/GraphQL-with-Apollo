f3b0bb3fd030e5b3d2a418ad97c45a05c8b0a58e
Zac <zacaree@gmail.com>
2018-10-22 11:59:22 -0500

    Fixed some styles and solved some ES Lint warnings.

--- src/Posts/EditMode.js
+++ src/Posts/EditMode.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import {ApolloConsumer} from 'react-apollo';
+import { ApolloConsumer } from 'react-apollo';
 
 export default class EditMode extends Component {
   render() {
-    const {isEditMode} = this.props;
+    const { isEditMode } = this.props;
     return (
       <ApolloConsumer>
         {client => (
+          // eslint-disable-next-line
           <a
             onClick={() => {
-              client.writeData({ data: { isEditMode: !isEditMode }});
+              client.writeData({ data: { isEditMode: !isEditMode } });
             }}
           >
-            ✏️ Toggle Edit Mode
+            <span role="img" aria-label="pencil icon">✏️</span>
+            Toggle Edit Mode
           </a>
         )}
       </ApolloConsumer>
--- src/Posts/Post.js
+++ src/Posts/Post.js
@@ -24,53 +24,57 @@ export default class Post extends Component {
                   <section>
                     <h1>{post.title}</h1>
                     <p>{post.body}</p>
-                    {/* Optimistic UI Example Below */}
-                    <Mutation
-                      // Here's our standatd mutation that talks to the GraphQL server.
-                      mutation={UPDATE_POST}
-                      variables={{
-                        id: post.id,
-                        check: !post.check
-                      }}
-                      // The optimistic response is the response you're expecting to get back from the above mutation.
-                      // You anticipate so that you can display the changes before the response comes back from the server.
-                      optimisticResponse={{
-                        __typename: 'Mutation',
-                        updatePost: {
-                          __typename: 'Post',
+                    <label htmlFor="Checkeroo">
+                      {/* Optimistic UI Example Below */}
+                      <Mutation
+                        // Here's our standatd mutation that talks to the GraphQL server.
+                        mutation={UPDATE_POST}
+                        variables={{
+                          id: post.id,
                           check: !post.check
-                        }
-                      }}
-                      update={(cache, { data: { updatePost } }) => {
-                        // Here we're reading the cache and getting the current data from the cache.
-                        const data = cache.readQuery({
-                          query: POST_QUERY,
-                          variables: {
-                            id: post.id
+                        }}
+                        // The optimistic response is the response you're expecting to get back from the above mutation.
+                        // You anticipate so that you can display the changes before the response comes back from the server.
+                        optimisticResponse={{
+                          __typename: 'Mutation',
+                          updatePost: {
+                            __typename: 'Post',
+                            check: !post.check
                           }
-                        });
-                        // Here we're modifying the value of the checkbox,
-                        // This value is coming into this function as an argument from updatePost above (from the optimisticResponse object).
-                        data.post.check = updatePost.check;
-                        // Now we need to write this current data into our Apollo cache
-                        cache.writeQuery({
-                          query: POST_QUERY,
-                          data: {
-                            ...data,
-                            post: data.post,
-                          }
-                        })
-                      }}
-                    >
-                      {updatePost => (
-                        <input
-                          style={{ zoom: '4' }}
-                          type="checkbox"
-                          checked={post.check}
-                          onChange={updatePost}
-                        />
-                      )}
-                    </Mutation>
+                        }}
+                        update={(cache, { data: { updatePost } }) => {
+                          // Here we're reading the cache and getting the current data from the cache.
+                          const data = cache.readQuery({
+                            query: POST_QUERY,
+                            variables: {
+                              id: post.id
+                            }
+                          });
+                          // Here we're modifying the value of the checkbox,
+                          // This value is coming into this function as an argument from updatePost above (from the optimisticResponse object).
+                          data.post.check = updatePost.check;
+                          // Now we need to write this current data into our Apollo cache
+                          cache.writeQuery({
+                            query: POST_QUERY,
+                            data: {
+                              ...data,
+                              post: data.post,
+                            }
+                          })
+                        }}
+                      >
+                        {updatePost => (
+                          <input
+                            style={{ zoom: '2' }}
+                            type="checkbox"
+                            id="Checkeroo"
+                            checked={post.check}
+                            onChange={updatePost}
+                          />
+                        )}
+                      </Mutation>
+                      Checkbox powered by Optimistic UI!
+                    </label>
                   </section>
                 )}
             </div>
